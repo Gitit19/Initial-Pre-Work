6ac8d396b22ca417a71a72d3925cbff848ce7c02
NorBitz <nj.heath@yahoo.com>
2019-05-10 06:33:04 -0600

    I made var a and var b equal 7.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,11 @@
+// I made var a and var b equal 7.
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a;
+a = 7; //The variable 'a' is equal to 7
+var b;
+b = 7; //The variable 'b' is equal to 7
+b = a;
--- 5-Introduction-to-JavaScript/Javascript.js
+++ 5-Introduction-to-JavaScript/Javascript.js
@@ -1,7 +1,17 @@
-// I created a variable.
+// I made var a and var b equal 7.
 // This is a in-line comment.
 /* This is a multi-line comment*/
 // Example
-var myName;
+// var myName;
 
 // Declare myName below this line
+// Setup
+// var a;
+// var b = 2;
+
+// Only change code below this line
+// var a;
+// a = 7; //The variable 'a' is equal to 7
+// var b;
+// b = 7; //The variable 'b' is equal to 7
+// b = a;
