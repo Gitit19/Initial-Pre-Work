d1d2675712d1a98658a7ebea0eaee09e8502955a
NorBitz <nj.heath@yahoo.com>
2019-05-13 05:11:51 -0600

    I created two new variables using myFirstName and myLastName.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+// I created two new variables using myFirstName and myLastName.
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Norman";
+var myLastName = "Heath";
