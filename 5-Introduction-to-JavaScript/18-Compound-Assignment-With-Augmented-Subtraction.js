c5fc664e4d15cce1cbe7d30af0beef41755cf595
NorBitz <nj.heath@yahoo.com>
2019-05-13 04:39:21 -0600

    I converted a, b, and c using -= opereator.

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,10 @@
+I convertrd a, b, and c using -= opereator.
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a = a -= 6; // Returns 5
+b = b -= 15; //Returns -6
+c = c -= 1; // Returns 2
