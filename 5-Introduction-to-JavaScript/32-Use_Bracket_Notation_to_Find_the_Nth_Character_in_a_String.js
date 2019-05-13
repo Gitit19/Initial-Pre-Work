bdb826c8a1c145a086cc4f13ada20b69174e6d9e
NorBitz <nj.heath@yahoo.com>
2019-05-13 13:23:09 -0600

    I set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,10 @@
+ // I set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+ // Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
