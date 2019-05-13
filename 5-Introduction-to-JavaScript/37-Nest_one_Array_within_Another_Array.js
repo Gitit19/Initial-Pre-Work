033da7d713a69beb8b5ef7adcda8da11f24e403f
NorBitz <nj.heath@yahoo.com>
2019-05-13 16:23:08 -0600

    I nested an array within an array.

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+// I nested an array within an array.
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Saints", 45], ["Broncos" , 17]];
