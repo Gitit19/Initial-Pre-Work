9e430e752fa24e9b5285a5bdbc23c4f90c3c341a
NorBitz <nj.heath@yahoo.com>
2019-05-13 04:57:10 -0600

    I converted the assignments for a, b, and c using /= operators.

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,10 @@
+// I converted the assignments for a, b, and c using /= operators.
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12; //Returns 4
+b /= 4; // Returns 27
+c /= 11; // Returns 3
