7ee85803f8077f3219c488b85d075d38715b90ff
NorBitz <nj.heath@yahoo.com>
2019-05-13 08:58:46 -0600

    I used backlashes to assign a string.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+// I used backlashes to assign a string.
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."//Change this line
---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,2 @@
+// I changed the double quotes to single quotes and removed all back slashes.
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
