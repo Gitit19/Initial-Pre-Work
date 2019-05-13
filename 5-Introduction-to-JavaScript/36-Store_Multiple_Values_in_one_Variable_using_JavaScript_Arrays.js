4ab0e1639066128e74e0a1e571b7a908af3f18e7
NorBitz <nj.heath@yahoo.com>
2019-05-13 15:27:09 -0600

    I modified the new array myArray so that it contains both a string and a number

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+// I modified the new array myArray so that it contains both a string and a number
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["John", 13];
