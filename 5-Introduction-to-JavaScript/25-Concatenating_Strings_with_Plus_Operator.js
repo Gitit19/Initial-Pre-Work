8c20f17e7bd54a5cd575bc70f48144940bbeae33
NorBitz <nj.heath@yahoo.com>
2019-05-13 10:10:55 -0600

    I built myStr variable from strings.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,7 @@
+I built a myStr variable from strings.
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. "+ "This is the end.";
