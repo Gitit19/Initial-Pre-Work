55cbee28239c6308594e9874226eb2cae364500a
NorBitz <nj.heath@yahoo.com>
2019-05-13 15:22:08 -0600

    I formed a complete sentence using the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb, then assigned the formed string to the result variable.

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,11 @@
+// I formed a complete sentence using the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb, then assigned the formed string to the result variable. 
+unction wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "My " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb; + ".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
