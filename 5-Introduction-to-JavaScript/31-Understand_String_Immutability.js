3bb9620f0dbbf6ebf922d3407f51a235847a1f82
NorBitz <nj.heath@yahoo.com>
2019-05-13 13:00:06 -0600

    I corrected the assignment to myStr so it contains the string value of Hello World.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+// I corrected the assignment to myStr so it contains the string value of Hello World.
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr= "Hello World"; // Fix Me
