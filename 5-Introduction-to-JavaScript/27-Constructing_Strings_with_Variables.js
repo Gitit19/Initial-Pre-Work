9759f26a90178cda290170c9f39c1e203a252912
NorBitz <nj.heath@yahoo.com>
2019-05-13 11:04:31 -0600

    I set myName to a string and build myStr with myName between the strings "My name is " and " and I am well!"

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+I set myName to a string and build myStr with myName between the strings "My name is " and " and I am well!"
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Norman Heath";
+var myStr = "My name is "+ myName + "and I am well!";
