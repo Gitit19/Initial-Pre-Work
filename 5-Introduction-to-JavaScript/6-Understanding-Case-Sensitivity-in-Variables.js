8d88eea92d3c41492944c7e08623d1c1a8a7aef5
NorBitz <nj.heath@yahoo.com>
2019-05-12 21:56:54 -0600

    I used lower case letters to start veriable declarations.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+I used lower case letters to start veriable declarations.
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
