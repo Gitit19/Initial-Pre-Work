3ef87ac289c5c8d8ff646dc3537268578d6f13e6
NorBitz <nj.heath@yahoo.com>
2019-05-13 04:33:02 -0600

    I converted the assignments for a, b, and c.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,10 @@
+I converted the assignments for a, b, and c.
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;// Returns 15
+b += 9; // Returns 26
+c += 7; // Returns 19
