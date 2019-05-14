2b4e738ebde4026aab78ce0d7a3619829f6c6a56
NorBitz <nj.heath@yahoo.com>
2019-05-13 23:56:44 -0600

    // I appended data to the end of the array.

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,6 @@
+// I appended data to the end of the array.
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push (["dog", 3])
+// myArray now equals ["John", "cat", ["dog", "3"]]
+// Only change code below this line.
