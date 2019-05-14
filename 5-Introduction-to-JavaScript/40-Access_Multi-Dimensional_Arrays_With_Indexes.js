7b4f3be892d6a39857539f104acf22d84307c206
NorBitz <nj.heath@yahoo.com>
2019-05-13 23:41:51 -0600

    // I used bracket notation to select an element from myArray so that myData is equal to 8.

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,6 @@
+// I used bracket notation to select an element from myArray so that myData is equal to 8.
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
